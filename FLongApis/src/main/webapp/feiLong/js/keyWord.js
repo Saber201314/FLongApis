682dc9b121519224c9b21e617057da63164472ab
镹丶天 <171553438@qq.com>
2017-05-16 18:57:21 +0800

    去除html代码

--- FLongApis/src/main/webapp/feiLong/js/keyWord.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-    $(".arrow_classify,.produc_name").click(function() {
-        $(this).siblings("ul").slideToggle(100);
-        $(this).children(".close_arr").toggleClass("open");
-        $(this).prev().children(".close_arr").toggleClass("open");
-    });
-})
